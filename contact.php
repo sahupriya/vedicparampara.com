6f16b9f6630a1c6919c490f3fe58d8786406e45b
unknown <neetusingh1995dec@gmail.com>
2020-08-10 13:20:32 +0530

    add

--- contact.php
+++ contact.php
@@ -8,6 +8,56 @@ if(isset($_POST['submit']))
 	$message=$_POST['message'];
 	$insert= "INSERT INTO contact (name, email, message) VALUES ('$name', '$email', '$message')";
 	$qyery=mysqli_query($conn,$insert);
+	if($query){
+		
+	$to = $email;
+    $from = "neetusingh1995dec@gmail.com";
+    $subject = "Thank Your for taking interest";
+    //begin of HTML message
+    $message = "Thank you for taking interest. One of our executive will contact you soon! <br>
+    Thanks & Regards,<br>
+    vedicparampara.com ";
+
+   //end of message
+    $headers  = "From: $from\r\n";
+    $headers .= "Content-type: text/html\r\n";
+
+    
+    // now lets send the email.
+    $message1 = "<html>
+<body>
+<table border='1' style='  border-collapse: collapse;'>
+<tr>
+<td>Name</td><td> ".$name."</td>
+</tr>
+<tr>
+<td>Email Id</td><td>".$email."</td>
+</tr>
+<tr>
+<td>Message</td><td>".$message."</td>
+</tr>
+</table>
+
+</body>
+</html>";
+    if(mail($to, $subject, $message, $headers)){
+        mail("neetsingh0612@gmail.com", "New Enquiry", $message1, $headers);
+?>
+    <script>
+        alert("Message sent!");
+    </script>
+<?php
+    }else{
+     ?>
+    <script>
+        alert("Unable to send message!");
+    </script>
+<?php   
+    }
+    
+}
+
+	}
 	?>
 	<script>
 	alert('your message send successfully');
